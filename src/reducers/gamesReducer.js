01bd3cc8ba238a1687bef9ebe70008ff0867f361
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-02-26 13:10:11 -0600

    api connection complete

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -10,16 +10,21 @@ const initState = {
 const gameReducer = (state=initState, action) => {
     switch(action.type){
         case "FETCH_GAMES":
-            return {...state}
-            default:
-                return {...state}
+            return { ...state,
+            popular: action.payload.popular,
+            upcoming: action.payload.upcoming,
+            newGames: action.payload.newGames }
+        default:
+            return { ...state }
     }
 };
 
-//Action
-{
-    type: "FETCH_GAMES";
-}
-dispatch({ type: "FETCH_GAMES" });
+//Action Creator
+const fetchGames = (userData) => {
+    return {
+        type: "FETCH_GAMES",
+        payload: userData,
+    };
+};
 
 export default gameReducer;
