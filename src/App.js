770791b141f5d5de3e5ee1b39ca7b9a4ef08a9dd
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-02-27 12:50:00 -0600

    global styles implemented

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {useDispatch} from 'react-redux';
 import { loadGames } from './actions/gamesActions';
 //Components and pages
 import Home from './pages/Home';
+import GlobalStyles from './components/GlobalStyles';
 
 function App() {
   return (
     <div className="App">
       <h1>GG</h1>
+      <Home />
     </div>
   );
 }
