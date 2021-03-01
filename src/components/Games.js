8a4f2f0abd8043aa74d42ab35eae0a1086d768e8
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-03-01 11:46:20 -0600

    fetching game details function complete

--- src/components/Games.js
+++ src/components/Games.js
@@ -3,13 +3,23 @@ import React from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import Home from '../pages/Home';
+//Redux
+import { useDispatch } from 'react-redux';
+import { loadDetails } from '../actions/detailAction';
 
-const Game = ({name, released, image}) => {
+const Game = ({ name, released, image, id }) => {
+    //Load Details
+    const dispatch = useDispatch();
+    const loadDetailHandler = () => {
+        dispatch(loadDetails(id));
+    }
     return(
+        <StyledGame onClick={loadDetailHandler}>
         <StyledGame>
             <h3>{name}</h3>
             <p>{released}</p>
             <img src={image} alt={name} />
+        </StyledGame>
         </StyledGame>
     );
 };
