b7ec8e10bbf8a5d7b3b733a09e0058d447e800ba
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-05-24 19:08:50 -0500

    preparing for deployment

--- src/util.js
+++ src/util.js
@@ -1,11 +1,15 @@
 //Media resize
 
 export const smallImage = (imagePath, size) => {
-  const image = imagePath.match(/media\/screenshots/)
-    ? imagePath.replace(
-        "media/screenshots",
-        `media/resize/${size}/-/screenshots`
-      )
-    : imagePath.replace("/media/games/", `/media/resize/${size}/-/games/`)
-  return image;
-};
+  let image;
+  if (imagePath) {
+    image = imagePath.match(/media\/screenshots/)
+      ? imagePath.replace(
+          "media/screenshots",
+          `media/resize/${size}/-/screenshots`
+        )
+      : imagePath.replace("/media/games", `/media/resize/${size}/-/games`);
+  } else {
+    image = imagePath;
+    return image;
+}}
